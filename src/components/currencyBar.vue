1931c8279619bf98d10cbefca41fae8c6fb8ae84
M2965 <M2965@student.jamk.fi>
2021-12-23 20:44:29 +0000

    Simple Date input

---
+++ src/components/currencyBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='valueBar'>
+        
+    </div>
+</template>
+<script lang+='ts'>
+import { defineComponent } from "@vue/runtime-core";
+
+
+export default defineComponent({
+    
+});
+
+</script>
+<style lang=""></style>
