d42deb12ceb6fced5d1b4f8a58f43c6b9f673745
M2965 <M2965@student.jamk.fi>
2021-12-30 12:27:36 +0000

    Working solution for trading volume

--- src/components/container.vue
+++ src/components/container.vue
@@ -3,38 +3,40 @@
     <div class="ui-small">
       <dateManager @dates="dateChanged" />
     </div>
-    <div class="ui-long">
-      <coin-history :startDate="startDate" :endDate="endDate" />
-    </div>
     <div class="content">
-      <coin-history :history="history" />
+      <coin-history :Prices="history.prices" />
+    </div>
+    <div class="stats">
+      <statistics v-if="historyLoaded == true" :history="history" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, reactive } from "vue";
+import { defineComponent, PropType } from "vue";
 import dateManager from "@/components/Datemanager.vue";
 import CoinHistory from "./CoinHistory.vue";
-
-import History from "../assets/market";
-import Coin from "@/type/Coin";
+import Statistics from "./statistics.vue";
+import History from "../type/market";
 
 export default defineComponent({
-  components: { dateManager, CoinHistory },
+  components: { dateManager, CoinHistory, Statistics },
   data() {
     return {
       startDate: 0,
       endDate: 0,
-      coins: [] as PropType<Coin>,
       history: [] as PropType<History>,
+      historyLoaded: false,
     };
   },
   methods: {
     dateChanged(start: number, end: number) {
+      // let gecko = new Gecko();
       this.startDate = start;
       this.endDate = end;
+      // this.history = gecko.fetchMarketChartRange("bitcoin", start, end);
       this.fetchMarketChartRange("bitcoin", this.startDate, this.endDate);
+      console.log(this.history);
     },
     fetchMarketChartRange(
       id: string,
@@ -44,13 +46,14 @@ export default defineComponent({
     ) {
       const apiAdress = "https://api.coingecko.com/api/v3/";
       const path = `/coins/${id}/market_chart/range/?vs_currency=${vs_currency}&from=${from}&to=${to}`;
+      // let hist: History;
       fetch(`${apiAdress + path}`).then((res) =>
         res
           .json()
-          .then((data) => (this.history = data.prices))
+          .then((data) => (this.history = data))
+          .then((history) => (this.historyLoaded = true))
           .catch((err) => console.log(err.message))
       );
-      // console.log(this.history);
     },
   },
 });
@@ -60,7 +63,10 @@ export default defineComponent({
 .grid-container {
   display: grid;
   gap: 1rem;
-  grid-template-columns: repeat(3, auto-fit, minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
   grid-auto-rows: 120px;
+}
+.content {
+  grid-column: span 2/2;
 }
 </style>
